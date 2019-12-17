8bb5f200c0c23a287e329b9dfef04128dc6cb0f9
rcmto <rick.mmartins3@gmail.com>
2019-12-17 17:37:31 -0300

    Anexo de arquivo

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,4 +1,8 @@
-class WhatsAppController {
+import {Format} from './../util/Format'
+import {CameraController} from './CameraController'
+import {DocumentPreviewController} from './DocumentPreviewController'
+
+export default class WhatsAppController {
 
     constructor() {
 
@@ -77,12 +81,12 @@ class WhatsAppController {
         }
         /*###########################     MUITO UTIL    #################################################
         */
-        HTMLFormElement.prototype.getForm = function (){
+        HTMLFormElement.prototype.getForm = function () {
 
             return new FormData(this);
         }
 
-        HTMLFormElement.prototype.toJSON = function(){
+        HTMLFormElement.prototype.toJSON = function () {
 
             let json = {};
 
@@ -138,13 +142,13 @@ class WhatsAppController {
 
         this.el.inputNamePanelEditProfile.on('keypress', e => {
 
-            if(e.key === 'Enter'){
+            if (e.key === 'Enter') {
                 e.preventDefault();
-                this.el.btnSavePanelEditProfile.click();        
+                this.el.btnSavePanelEditProfile.click();
             }
         });
 
-        this.el.btnSavePanelEditProfile.on('click', e=>{
+        this.el.btnSavePanelEditProfile.on('click', e => {
 
             console.log(this.el.inputNamePanelEditProfile.innerHTML);
         });
@@ -167,8 +171,8 @@ class WhatsAppController {
         });
 
         this.el.btnAttach.on('click', e => {
-            
-            e.stopPropagation();0
+
+            e.stopPropagation(); 0
             this.el.menuAttach.toggleClass('open');
             document.addEventListener('click', this.closeMenuAttach.bind(this));
 
@@ -196,6 +200,45 @@ class WhatsAppController {
             this.el.panelDocumentPreview.css({
                 height: "100%",
             });
+            this.el.inputDocument.click();
+        });
+
+        this.el.inputDocument.on('change', e => {
+
+            if(this.el.inputDocument.files.length){
+                let file = this.el.inputDocument.files[0];
+                
+                this._documentPreviewController = new DocumentPreviewController(file);
+
+                this._documentPreviewController.getPreviewData().then(result => {
+
+                    this.el.imgPanelDocumentPreview.src = result.src;
+                    this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                    this.el.imagePanelDocumentPreview.show();
+                    this.el.filePanelDocumentPreview.hide();
+
+                }).catch(err => {
+                    switch(file.type){
+                        case 'application/vnd.ms-excel':
+                        case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls';
+                            break;
+                        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                        case 'application/msword':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc';
+                            break;
+                        case 'text/plain':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-txt';
+                            break;
+                        default:
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic';
+                            break;
+                    }
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                    this.el.imagePanelDocumentPreview.hide();
+                    this.el.filePanelDocumentPreview.show();
+                });
+            }
         });
 
         this.el.btnClosePanelDocumentPreview.on('click', e => {
@@ -218,18 +261,48 @@ class WhatsAppController {
             this.el.panelCamera.css({
                 height: "100%",
             });
+
+            this._camera = new CameraController(this.el.videoCamera);
+            this.el.videoCamera.show();
+            this.el.containerSendPicture.hide();
+            this.el.containerTakePicture.show();
+            this.el.btnReshootPanelCamera.hide();
+
         });
 
         this.el.btnClosePanelCamera.on('click', e => {
 
             this.el.panelCamera.removeClass('open');
             this.el.panelMessagesContainer.show();
+            this._camera.stop();
+            this.el.pictureCamera.hide();
 
         });
 
         this.el.btnTakePicture.on('click', e => {
 
-            console.log('tirando foto');
+            let dataUrl = this._camera.takePicture();
+
+            this.el.pictureCamera.src = dataUrl;
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();
+
+        });
+
+        this.el.btnReshootPanelCamera.on('click', e => {
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+        });
+
+        this.el.btnSendPicture.on('click', e =>{
+
+            console.log(this.el.pictureCamera.src);
         });
 
         this.el.btnAttachContact.on('click', e => {
@@ -256,36 +329,111 @@ class WhatsAppController {
         });
 
         this.el.btnFinishMicrophone.on('click', e => {
-            
+
             this.closeRecordMicrophone();
         });
+
+        this.el.inputText.on('keyup', e => {
+
+            if (this.el.inputText.innerHTML.length) {
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+            } else {
+                this.el.inputPlaceholder.show();
+                this.el.btnSend.hide();
+                this.el.btnSendMicrophone.show();
+
+            }
+        });
+
+        this.el.inputText.on('keypress', e => {
+
+            if (e.key === 'Enter' && !e.ctrlKey) {
+
+                e.preventDefault();
+                this.el.btnSend.click();
+            }
+        });
+
+        this.el.btnSend.on('click', e => {
+
+            console.log(this.el.inputText.innerHTML);
+        });
+
+        this.el.btnEmojis.on('click', e => {
+
+            this.el.panelEmojis.toggleClass('open');
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+
+            emoji.on('click', e => {
+
+                let img = this.el.imgEmojiDefault.cloneNode();
+
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+
+                emoji.classList.forEach(name => {
+
+                    img.classList.add(name);
+                });
+
+                let cursor = window.getSelection();
+
+                if(!cursor.focusNode || !cursor.focusNode.id == 'input-text'){
+                    this.el.inputText.focus();
+                    cursor = window.getSelection();
+                }
+
+                let range = document.createRange();
+
+                range = cursor.getRangeAt(0);
+
+                range.deleteContents();
+
+                let frag = document.createDocumentFragment();
+
+                frag.appendChild(img);
+
+                range.insertNode(frag);
+
+                range.setStartAfter(img);
+
+                this.el.inputText.dispatchEvent(new Event('keyup'));
+            });
+        });
+
+        
     }
 
-    startRecordMicrophoneTime(){
+    startRecordMicrophoneTime() {
         let start = Date.now();
 
-        this._recordMicrophoneInterval = setInterval(()=>{
+        this._recordMicrophoneInterval = setInterval(() => {
 
-            this.el.recordMicrophoneTimer.innerHTML = (Date.now() - start); 
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
 
         }, 100);
     }
 
-    closeRecordMicrophone(){
+    closeRecordMicrophone() {
 
         this.el.recordMicrophone.hide();
         this.el.btnSendMicrophone.show();
         clearInterval(this._recordMicrophoneInterval);
     }
 
-    closeAllMainPanel(){
-        
+    closeAllMainPanel() {
+
         this.el.panelMessagesContainer.hide();
         this.el.panelDocumentPreview.removeClass('open');
         this.el.panelCamera.removeClass('open');
     }
 
-    closeMenuAttach(e){
+    closeMenuAttach(e) {
 
         document.removeEventListener('click', this.closeMenuAttach);
         this.el.menuAttach.removeClass('open');
--- src/util/Format.js
+++ src/util/Format.js
@@ -1,4 +1,4 @@
-class Format {
+export class Format {
     
     /*
     * Essa função recebe o id dos objetos separados por '-' ex: list-button-xx
@@ -12,5 +12,18 @@ class Format {
         div.innerHTML = `<div data-${text}="id"></div>`;
 
         return Object.keys(div.firstChild.dataset)[0];
+    }
+
+    static toTime(duration){
+
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if(hours > 0){
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        }else{
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+        }
     }
 }
